86b1326e30fcf721f6234870c70278c31f83ff2d
Dante Catalfamo <dante.catalfamo@gmail.com>
2021-02-02 01:59:49 -0500

    Alphabetical order usage flags

--- pjail.c
+++ pjail.c
@@ -105,7 +105,7 @@ main(int argc, char **argv)
 void
 usage(void)
 {
-    printf("pjail: [-hvli] [-p promise] [command] [args...]\n");
+    printf("pjail: [-hilv] [-p promise] [command] [args...]\n");
 }
 
 void
